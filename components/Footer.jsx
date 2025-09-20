5fc54647768dd27089929b29674f30f2c760df57
koech161 <koechabel161@gmail.com>
2025-09-20 10:35:46 +0300

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -147,18 +147,7 @@ export default function Footer() {
                 </svg>
                 High Altitude Training Center, Iten
               </p>
-              <p className="flex items-start">
-                <svg className="w-4 h-4 mt-1 mr-3 text-zinc-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                120-140km weekly mileage
-              </p>
-              <p className="flex items-start">
-                <svg className="w-4 h-4 mt-1 mr-3 text-zinc-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-                Sponsored by Nike Kenya
-              </p>
+              
             </div>
           </motion.div>
         </div>

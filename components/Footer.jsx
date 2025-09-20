ba8afad658662923e0caf38fa11170ebd85bef04
koech161 <koechabel161@gmail.com>
2025-09-20 10:58:38 +0300

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -123,13 +123,7 @@ export default function Footer() {
                 </svg>
                 +254 700 000 000
               </p>
-              <p className="flex items-center">
-                <svg className="w-4 h-4 mr-3 text-zinc-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-                Iten, Kenya
-              </p>
+             
             </div>
           </motion.div>
 

1befebaacc6a9c93fb9318d75d1f33ab19655a2b
koech161 <koechabel161@gmail.com>
2025-09-24 17:38:56 +0300

    update contact info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -122,7 +122,7 @@ export default function Footer() {
                 <svg className="w-4 h-4 mr-3 text-lime-400" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M6.62 10.79c1.44 2.83 3.76 5.15 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.12.35.03.74-.24 1.02l-2.2 2.2z"/>
                 </svg>
-                +254 701 848v 586
+                +254 701 848 586
               </p>
               <p className="flex items-center">
                 <svg className="w-4 h-4 mr-3 text-lime-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
